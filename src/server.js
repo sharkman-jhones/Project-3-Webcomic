d5ecdc824ac7dabe297d9ae869fc7006bcc4540a
Armando Morales <Armando Morales>
2017-01-31 15:36:53 -0600

    corrected small error on server file

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import Express from 'express';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { match, RouterContext } from 'react-router'
-import routes from '.routes';
+import routes from './routes';
 import NotFoundPage from './components/NotFoundPage';
 
 const app = new Express();
